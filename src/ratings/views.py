32874b87b58a8c910febb2659a648aea3bbf7c85
BobLouis <bob900316@gmail.com>
2023-12-11 18:31:10 +0800

    up

--- src/ratings/views.py
+++ src/ratings/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.contenttypes.models import ContentType
+from django.views.decorators.http import require_http_methods
+from .models import Rating
 
-# Create your views here.
+
+@require_http_methods(['POST'])
+def rate_movie_view(request):
+    if not request.htmx:
+        return HttpResponse("Not allowed", status=400)
+    object_id = request.POST.get('object_id')
+    rating_value = request.POST.get('rating_value')
+    user =  request.user
+    message = "You must <a href='/accounts/login/'>login</a> to rate this movie."
+    if user.is_authenticated:
+       message = "<span class='bg-danger text-light px-3 rounded'>error occur</span>"
+       ctype = ContentType.objects.get(app_label='movies', model='movie')
+       rating_obj = Rating.objects.create(content_type=ctype, object_id=object_id, value=rating_value,user=user)
+       if rating_obj.content_object is not None:
+           message = "<span class='bg-success text-light px-3 rounded'>Rating saved !</span>"
+    return HttpResponse(message, status=200)
